a3cbd0185e5a4d15313852743422b917addef40f
Tato <tatomorales0980@gmail.com>
2020-10-07 13:53:30 -0400

    Initial commit

---
+++ app/src/main/java/com/tato0980/familytasks/LottieAnim.kt
@@ -0,0 +1,11 @@
+package com.tato0980.familytasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LottieAnim : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lottie_anim)
+    }
+}
---
+++ app/src/main/java/com/tato0980/familytasks/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.tato0980.familytasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+//no. screens
+
+//login
+//signup
+//homescreen
+//design screens
+//structure of db
